6b37fd12e30fcdd61f221b90d3b0237d8d55adeb
Shelepov G <gvshelepov@gmail.com>
2018-07-17 12:44:28 +0300

    ci 1 refs #3

---
+++ tests/TestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase {
+
+    public $fixtureUrl;
+
+    protected function setUp()
+    {
+
+    }
+
+    public function testConstuctor() {
+
+        $this->assertTrue(true);
+    }
+}
