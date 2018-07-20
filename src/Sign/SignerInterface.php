0b45bdc7350d2a95b8a17e8151893aa30be9dff8
Shelepov G <gvshelepov@gmail.com>
2018-07-20 19:55:54 +0300

    SignerInterface extend refs #4

--- src/Sign/SignerInterface.php
+++ src/Sign/SignerInterface.php
@@ -9,4 +9,10 @@ interface SignerInterface
      * @return String
      */
     public function getSignature(array $data);
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    public  function arrayToString(array $data);
 }
