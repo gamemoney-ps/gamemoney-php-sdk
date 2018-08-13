acfb06a7ee582fcd25460da11e00901ccc098f55
Shelepov G <gvshelepov@gmail.com>
2018-08-13 19:36:53 +0300

    fix phpdoc refs #10

--- src/Request/RequestInterface.php
+++ src/Request/RequestInterface.php
@@ -22,12 +22,12 @@ interface RequestInterface
     const STATISTICS_PAY_TYPES_ACTION = '/statistics/paytypes';
 
     /**
-     * @return string response uri
+     * @return string request uri
      */
     public function getAction();
 
     /**
-     * @return array response data fields
+     * @return array request data fields
      */
     public function getData();
 
