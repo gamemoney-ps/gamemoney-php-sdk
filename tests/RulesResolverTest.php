7fa17c8110f76e90d350384d108ef9ef4d767e77
vestnik <kopychev87@csgo.com>
2019-07-08 12:22:32 +0300

    fix rules resolver test

--- tests/RulesResolverTest.php
+++ tests/RulesResolverTest.php
@@ -15,25 +15,26 @@ class RulesResolverTest extends TestCase
     public function resolveProvider()
     {
         return [
-            ['action' => RequestInterface::INVOICE_CREATE_ACTION],
-            ['action' => RequestInterface::INVOICE_STATUS_ACTION],
-            ['action' => RequestInterface::INVOICE_LIST_ACTION],
-            ['action' => RequestInterface::CHECKOUT_CANCEL_ACTION],
-            ['action' => RequestInterface::CHECKOUT_CREATE_ACTION],
-            ['action' => RequestInterface::CHECKOUT_STATUS_ACTION],
-            ['action' => RequestInterface::CHECKOUT_LIST_ACTION],
-            ['action' => RequestInterface::EXCHANGE_CONVERT_ACTION],
-            ['action' => RequestInterface::EXCHANGE_FAST_CONVERT_ACTION],
-            ['action' => RequestInterface::EXCHANGE_INFO_ACTION],
-            ['action' => RequestInterface::EXCHANGE_PREPARE_ACTION],
-            ['action' => RequestInterface::EXCHANGE_STATUS_ACTION],
-            ['action' => RequestInterface::CARD_LIST_ACTION],
-            ['action' => RequestInterface::CARD_ADD_ACTION],
-            ['action' => RequestInterface::CARD_DELETE_ACTION],
-            ['action' => RequestInterface::STATISTICS_BALANCE_ACTION],
-            ['action' => RequestInterface::STATISTICS_DAYS_BALANCE_ACTION],
-            ['action' => RequestInterface::STATISTICS_PAY_TYPES_ACTION],
-            ['action' => 'wrong action'],
+            ['action' => RequestInterface::INVOICE_CREATE_ACTION, 'data' => []],
+            ['action' => RequestInterface::INVOICE_STATUS_ACTION, 'data' => ['invoice' => 123]],
+            ['action' => RequestInterface::INVOICE_STATUS_ACTION, 'data' => ['project_invoice' => 'test']],
+            ['action' => RequestInterface::INVOICE_LIST_ACTION, 'data' => []],
+            ['action' => RequestInterface::CHECKOUT_CANCEL_ACTION, 'data' => []],
+            ['action' => RequestInterface::CHECKOUT_CREATE_ACTION, 'data' => []],
+            ['action' => RequestInterface::CHECKOUT_STATUS_ACTION, 'data' => []],
+            ['action' => RequestInterface::CHECKOUT_LIST_ACTION, 'data' => []],
+            ['action' => RequestInterface::EXCHANGE_CONVERT_ACTION, 'data' => []],
+            ['action' => RequestInterface::EXCHANGE_FAST_CONVERT_ACTION, 'data' => []],
+            ['action' => RequestInterface::EXCHANGE_INFO_ACTION, 'data' => []],
+            ['action' => RequestInterface::EXCHANGE_PREPARE_ACTION, 'data' => []],
+            ['action' => RequestInterface::EXCHANGE_STATUS_ACTION, 'data' => []],
+            ['action' => RequestInterface::CARD_LIST_ACTION, 'data' => []],
+            ['action' => RequestInterface::CARD_ADD_ACTION, 'data' => []],
+            ['action' => RequestInterface::CARD_DELETE_ACTION, 'data' => []],
+            ['action' => RequestInterface::STATISTICS_BALANCE_ACTION, 'data' => []],
+            ['action' => RequestInterface::STATISTICS_DAYS_BALANCE_ACTION, 'data' => []],
+            ['action' => RequestInterface::STATISTICS_PAY_TYPES_ACTION, 'data' => []],
+            ['action' => 'wrong action', 'data' => []],
         ];
     }
 
@@ -41,10 +42,11 @@ class RulesResolverTest extends TestCase
      * @param string $action
      * @dataProvider resolveProvider
      */
-    public function testResolve($action)
+    public function testResolve($action, array $data)
     {
         $resolver = new RulesResolver();
-        $rules = $resolver->resolve($action);
+        $rules = $resolver->resolve($action, $data);
+
         $this->assertInstanceOf(RulesInterface::class, $rules);
 
         $this->assertInternalType('array', $rules->getRules());
@@ -53,6 +55,6 @@ class RulesResolverTest extends TestCase
     public function testWrongActionResolve()
     {
         $resolver = new RulesResolver();
-        $this->assertInstanceOf(DefaultRules::class, $resolver->resolve('wrong action'));
+        $this->assertInstanceOf(DefaultRules::class, $resolver->resolve('wrong action', []));
     }
 }
