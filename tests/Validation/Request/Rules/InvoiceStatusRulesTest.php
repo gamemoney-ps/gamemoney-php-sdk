cadfab032d579126c7fc10b9d244ce1ccdbc3386
vestnik <kopychev87@csgo.com>
2019-07-08 15:52:56 +0300

    add InvoiceStatusRules test

---
+++ tests/Validation/Request/Rules/InvoiceStatusRulesTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace tests\Validation\Request\Rules;
+
+use Gamemoney\Validation\Request\Rules\InvoiceStatusRules;
+use PHPUnit\Framework\TestCase;
+
+
+class InvoiceStatusRulesTest extends TestCase
+{
+    /**
+    * @return array
+    */
+    public function invoiceStatusProvider()
+    {
+        return [
+            ['checkField' => ['project', 'rand', 'invoice'], 'data' => ['project' => 123456, 'rand' => '56f97b6c07d66cde3d20', 'invoice' => 1]],
+            ['checkField' => ['project', 'rand', 'invoice'], 'data' => ['project' => 123456, 'rand' => '56f97b6c07d66cde3d20', 'invoice' => 1, 'project_invoice' => 'test']],
+            ['checkField' => ['project', 'rand', 'project_invoice'], 'data' => ['project' => 123456, 'rand' => '56f97b6c07d66cde3d20', 'project_invoice' => 'test']],
+        ];
+    }
+
+    /**
+     * @param array $check
+     * @param array $data
+     * @dataProvider invoiceStatusProvider
+     */
+    public function testInvoiceStatus(array $checkField, array $data)
+    {
+        $invoice = new InvoiceStatusRules($data);
+
+        foreach ($checkField as $item) {
+            $this->assertArrayHasKey($item, $invoice->getRules());
+        }
+    }
+}
